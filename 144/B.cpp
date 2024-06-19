deb9f7c8774f2d77e189da25b5b6b586e0ba6eec
tatuki kosikawa <159102831+tatu-coding@users.noreply.github.com>
2024-06-19 21:11:40 +0900

    6/19 last

--- 144/B.cpp
+++ 144/B.cpp
@@ -3,5 +3,25 @@
 using namespace std;
 
 int main() {
-    
+    int num;
+    cin >> num;
+
+    bool found = false;
+
+    for (int i = 0; i <= 9; i++) {
+        for (int j = 0; j <= 9; j++) {
+            if (i * j == num) {
+                cout << "Yes" << endl;
+                found = true;
+                break;
+            }
+        }
+        if (found == true) {
+            break;
+        }
+    }
+
+    if (found == false) {
+        cout << "No" << endl;
+    }
 }
