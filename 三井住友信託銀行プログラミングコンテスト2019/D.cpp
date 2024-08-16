cb3779d44509b2cd60630c6f05afef37b73ece3f
tatuki kosikawa <159102831+tatu-coding@users.noreply.github.com>
2024-08-16 19:08:05 +0900

    8/16

--- 三井住友信託銀行プログラミングコンテスト2019/D.cpp
+++ 三井住友信託銀行プログラミングコンテスト2019/D.cpp
@@ -1,31 +1,61 @@
 #include <iostream>
-#include <vector>
 #include <string>
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
-
     string str;
     cin >> str;
+    
+    int ans = 0;
 
-    vector<int> num(n);
+    for (int i = 0; i < 1000; i++) {
 
-    for (int i = 0; i < n; i++) {
-        string str_tem = str.erase(i, 1);
-        num[i] = stoi(str_tem);
-    }
+        int x = i;
+        string str2;
+        for (int j = 0; j < 3; j++) {
+            str2 += '0' + x%10;
+            x /= 10;
+        }
 
-    int count;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (num[i] == num[j]) {
-                ;
+        int y = 0;
+        for (int k = 0; k < n; k++) {
+            if (str[k] == str2[y]) {
+                y++;
+                if (y == 3) {
+                    break;
+                }
             }
+        }
+
+        if (y == 3) {
+            ans++;
         }
     }
 
-
+    cout << ans << endl;
 }
+
+    // for (int i = 0; i < n - 2; i++) {
+    //     for (int j = i + 1; j < n - 1; j++) {
+    //         for (int k = j + 1; k < n; k++) {
+    //             string str2;
+    //             str2.push_back(str[i]);
+    //             str2.push_back(str[j]);
+    //             str2.push_back(str[k]);
+    //             sub_str.push_back(str2);
+    //         }
+    //     }
+    // }
+
+    // for (int i = 0; i < sub_str.size(); i++) {
+    //     for (int j = i + 1; j < sub_str.size(); j++) {
+    //         if (sub_str[i] == sub_str[j]) {
+    //             sub_str.erase(sub_str.begin() + j);
+    //             j--;
+    //         }
+    //     }
+    // }
+
+    // cout << sub_str.size() << endl;
